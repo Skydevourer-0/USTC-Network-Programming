d15b8bef36c239f882fc355a4ddcab2dccc75427
skydevourer <skydevourer@foxmail.com>
2023-12-14 22:27:08 +0800

    前端传入的数据只有一个变量，那么通过 req.body 获取时，也不能使用大括号进行解析

--- Lab1/online_groupchat_server/routes/messages.js
+++ Lab1/online_groupchat_server/routes/messages.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // 获取消息记录
 router.get('/load-messages', (req, res) => {
-    const {date} = req.body;
+    const date = req.body;
     const file = path.join(__dirname, '..', 'public', 'messageList.json');
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
@@ -20,7 +20,8 @@ router.get('/load-messages', (req, res) => {
 
 // 存储消息记录
 router.post('/save-messages', (req, res) => {
-    const {messages} = req.body;
+    const messages = req.body;
+    console.log(messages)
     const file = path.join(__dirname, '..', 'public', 'messageList.json');
     fs.readFile(file, 'utf-8', (err, data) => {
         if (err) {
